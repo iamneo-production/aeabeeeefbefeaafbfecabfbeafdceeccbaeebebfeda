ab5649e33b177a8267e789fa5d28a3170aa66a88
Iamneo <techadmin@iamneo.ai>
2023-08-31 09:54:20 +0000

    Backup Commit

--- angularapp/src/app/new-food/new-food.component.ts
+++ angularapp/src/app/new-food/new-food.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Food } from '../interface/food';
+import { items } from '../interface/foods';
 
 @Component({
   selector: 'app-new-food',
   templateUrl: './new-food.component.html',
   styleUrls: ['./new-food.component.css']
 })
-export class NewFoodComponent {
+export class NewFoodComponent implements OnInit {
+
+  items: Food[]=[];
+  newFoodName = 'Enter Food Name';
+  newFoodCalories = 0;
+  newFoodImage = '';
+
+  constructor() {
+    this.items=[];
+   }
+
+  ngOnInit(): void {
+    this.items = items;
+  }
+
+  addFood()
+  {
+    const newFood = {
+      name: this.newFoodName,
+      calories: this.newFoodCalories,
+      image: this.newFoodImage,
+      count: 0
+    };
+
+    this.items.unshift(newFood);
+  }
+
+  addCount(count: any, item: { count: any; }): any{
+    item.count += count;
+  }
 
 }
