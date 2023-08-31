7b2662ea52a21a0d69aca88ad714ca21bf0c463a
Iamneo <techadmin@iamneo.ai>
2023-08-31 10:20:59 +0000

    Backup Commit

---
+++ angularapp/src/app/today/today.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from './../interface/food';
+import { items } from './../interface/foods';
+
+@Component({
+  selector: 'app-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.css']
+})
+export class TodayComponent implements OnInit {
+  items: Food[]=[];
+  newFood: any = {};
+
+  constructor() {
+    this.items=[];
+  }
+
+  ngOnInit(): void {
+    this.items = items;
+  }
+
+  addCount(count: any, item: { count: any; }): any {
+    item.count += count;
+  }
+
+
+}
